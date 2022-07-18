c4ab4cbe3c804d3abb9835f83585e526eccb616b
Waldemar Kuranc <wkuranc@gmail.com>
2022-07-18 20:09:58 +0200

    add post model

--- pywww/posts/models.py
+++ pywww/posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    # pole tekstowe o określonej długości
+    content = models.TextField()
+    # pole tekstowe o nieokreślonej długości
+    published = models.BooleanField(default=False)
+    # flaga true/false
+    created = models.DateTimeField(auto_now_add=True)
+    # data utworzenia - tylko przy utworzeniu
+    modified = models.DateTimeField(auto_now=True)
+    # data modyfikacji - zawsze gdy klikniemy save
+    
+
--- pywww/pywww/settings.py
+++ pywww/pywww/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
